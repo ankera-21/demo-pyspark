ef86ef997550b1eaa53d8289e035c419c6d3e56e
ankera-21 <ankita13mun@gmail.com>
2025-02-11 22:17:43 -0600

    Update 2.py

--- 2.py
+++ 2.py
@@ -35,3 +35,48 @@ final_counts AS (
     FROM reboot_counts
 )
 SELECT * FROM final_counts;
+
+
+----
+
+
+-- Optimized Version
+WITH parsed_data AS (
+    SELECT
+        *,
+        TIMESTAMP_SUB(
+            INFORM_TIME, 
+            INTERVAL CAST(SPLIT(uptime, ' ')[0] AS INT64) DAY
+        ) AS temp_time
+    FROM df_tbl
+),
+parsed_data_final AS (
+    SELECT
+        *,
+        TIMESTAMP_SUB(
+            TIMESTAMP_SUB(
+                TIMESTAMP_SUB(
+                    temp_time,
+                    INTERVAL CAST(SPLIT(SPLIT(uptime, ' ')[1], ':')[0] AS INT64) HOUR
+                ),
+                INTERVAL CAST(SPLIT(SPLIT(uptime, ' ')[1], ':')[1] AS INT64) MINUTE
+            ),
+            INTERVAL CAST(SPLIT(SPLIT(uptime, ' ')[1], ':')[2] AS INT64) SECOND
+        ) AS REBOOT_TIME
+    FROM parsed_data
+),
+reboot_counts AS (
+    SELECT
+        INFORM_DATE, INFORM_HR, DEVICE_ID, uptime, REBOOT_TIME,
+        COUNT(DISTINCT REBOOT_TIME) OVER (
+            PARTITION BY INFORM_DATE, INFORM_HR, DEVICE_ID
+        ) AS REBOOT_COUNT
+    FROM parsed_data_final
+)
+SELECT
+    *,
+    IF(uptime LIKE '0000 00%', REBOOT_COUNT, 0) AS ADJ_REBOOT_COUNT,
+    MAX(IF(uptime LIKE '0000 00%', REBOOT_COUNT, 0)) OVER (
+        PARTITION BY INFORM_DATE, INFORM_HR, DEVICE_ID
+    ) AS MAX_REBOOT_COUNT
+FROM reboot_counts;
