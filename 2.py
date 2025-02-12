328639857dca6b738ff97ebfda51e95c13c85be2
ankera-21 <ankita13mun@gmail.com>
2025-02-11 22:17:03 -0600

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,37 @@
+-- Most Earlier Longer Version
+WITH parsed_data AS (
+    SELECT
+        *,
+        TIMESTAMP_SUB(
+            TIMESTAMP_SUB(
+                TIMESTAMP_SUB(
+                    TIMESTAMP_SUB(
+                        INFORM_TIME, 
+                        INTERVAL CAST(SPLIT(uptime, ' ')[0] AS INT64) DAY
+                    ), 
+                    INTERVAL CAST(SPLIT(SPLIT(uptime, ' ')[1], ':')[0] AS INT64) HOUR
+                ), 
+                INTERVAL CAST(SPLIT(SPLIT(uptime, ' ')[1], ':')[1] AS INT64) MINUTE
+            ), 
+            INTERVAL CAST(SPLIT(SPLIT(uptime, ' ')[1], ':')[2] AS INT64) SECOND
+        ) AS REBOOT_TIME
+    FROM df_tbl
+),
+reboot_counts AS (
+    SELECT
+        *,
+        COUNT(DISTINCT REBOOT_TIME) OVER (
+            PARTITION BY INFORM_DATE, INFORM_HR, DEVICE_ID
+        ) AS REBOOT_COUNT
+    FROM parsed_data
+),
+final_counts AS (
+    SELECT
+        *,
+        CASE WHEN uptime LIKE '0000 00%' THEN REBOOT_COUNT ELSE 0 END AS ADJ_REBOOT_COUNT,
+        MAX(CASE WHEN uptime LIKE '0000 00%' THEN REBOOT_COUNT ELSE 0 END) OVER (
+            PARTITION BY INFORM_DATE, INFORM_HR, DEVICE_ID
+        ) AS MAX_REBOOT_COUNT
+    FROM reboot_counts
+)
+SELECT * FROM final_counts;
